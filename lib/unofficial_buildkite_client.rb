0ec6a4fb9949d53c153031638e477a20a7db5ee6
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2019-04-23 11:55:06 +0900

    $ bundle gem unofficial_buildkite_client

---
+++ lib/unofficial_buildkite_client.rb
@@ -0,0 +1,6 @@
+require "unofficial_buildkite_client/version"
+
+module UnofficialBuildkiteClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/unofficial_buildkite_client/version.rb
@@ -0,0 +1,3 @@
+module UnofficialBuildkiteClient
+  VERSION = "0.1.0"
+end
